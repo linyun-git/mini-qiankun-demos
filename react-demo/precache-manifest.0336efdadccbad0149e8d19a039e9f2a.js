bdc977317e092d5797e10b57f5826de77b2ccc7f
linyun <hly.baifeng@qq.com>
2021-11-21 13:43:48 +0800

    Updates

---
+++ react-demo/precache-manifest.0336efdadccbad0149e8d19a039e9f2a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "582ded5d3677bd347f75938e9c599dc7",
+    "url": "/mini-qiankun-demos/react-demoindex.html"
+  },
+  {
+    "revision": "573e5ae770bd19e0faec",
+    "url": "/mini-qiankun-demos/react-demostatic/css/2.4eb19277.chunk.css"
+  },
+  {
+    "revision": "a8d5a37aba336d0cc14c",
+    "url": "/mini-qiankun-demos/react-demostatic/css/main.c9be9530.chunk.css"
+  },
+  {
+    "revision": "573e5ae770bd19e0faec",
+    "url": "/mini-qiankun-demos/react-demostatic/js/2.adf1b35b.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/mini-qiankun-demos/react-demostatic/js/2.adf1b35b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1ed795846ff95054f2e",
+    "url": "/mini-qiankun-demos/react-demostatic/js/3.7a1df24a.chunk.js"
+  },
+  {
+    "revision": "a8d5a37aba336d0cc14c",
+    "url": "/mini-qiankun-demos/react-demostatic/js/main.dddfd3b9.chunk.js"
+  },
+  {
+    "revision": "6c17eed6886001d05f07",
+    "url": "/mini-qiankun-demos/react-demostatic/js/runtime-main.c8e7bcb4.js"
+  }
+]);
---
+++ react-demo/static/js/3.7a1df24a.chunk.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp_react-demo"]=window["webpackJsonp_react-demo"]||[]).push([[3],{167:function(e,n,t){"use strict";t.r(n);var a=t(0),o=t.n(a);n.default=function(){return o.a.createElement("h2",{className:"app-nav-item",style:{borderColor:"green"}},"About")}}}]);
+//# sourceMappingURL=3.7a1df24a.chunk.js.map
