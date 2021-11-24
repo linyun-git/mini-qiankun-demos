4501b8125f4050cd6307c4d01785197e01bea7c0
linyun <hly.baifeng@qq.com>
2021-11-24 22:50:18 +0800

    Updates

---
+++ react-demo/precache-manifest.e516740720d07118886c5a4eb6110721.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "182be08a1efde69e973aa2be0f4500e8",
+    "url": "/mini-qiankun-demos/react-demo/index.html"
+  },
+  {
+    "revision": "e22754116dae4a5b8439",
+    "url": "/mini-qiankun-demos/react-demo/static/css/2.4eb19277.chunk.css"
+  },
+  {
+    "revision": "4399e336313978a13acd",
+    "url": "/mini-qiankun-demos/react-demo/static/css/main.c9be9530.chunk.css"
+  },
+  {
+    "revision": "e22754116dae4a5b8439",
+    "url": "/mini-qiankun-demos/react-demo/static/js/2.65bf9885.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/mini-qiankun-demos/react-demo/static/js/2.65bf9885.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0501645c9a21d08c9ba1",
+    "url": "/mini-qiankun-demos/react-demo/static/js/3.1251e366.chunk.js"
+  },
+  {
+    "revision": "4399e336313978a13acd",
+    "url": "/mini-qiankun-demos/react-demo/static/js/main.c5d4245f.chunk.js"
+  },
+  {
+    "revision": "b96b741f6b3c47f6e7e8",
+    "url": "/mini-qiankun-demos/react-demo/static/js/runtime-main.08b75d31.js"
+  }
+]);
