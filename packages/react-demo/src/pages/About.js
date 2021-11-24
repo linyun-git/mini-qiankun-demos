8dd9a8920a10bc70d193ee90789f1c4c64c7ff29
linyun <linyun.hu@.hand-china.com>
2021-11-24 20:46:51 +0800

    update

--- packages/react-demo/src/pages/About.js
+++ packages/react-demo/src/pages/About.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function() {
+  const [mock, setMock] = useState(window.mock || '');
+  const [windowMock, setWindowMock] = useState(window.mock || '');
+
+  const handleClick = () => {
+    window.mock = mock;
+    setWindowMock(mock);
+  }
+
   return (
     <h2 className="app-nav-item" style={{ borderColor: 'green' }}>
       About
+      <div>
+        <code>window.mock=</code>
+        <input type="text" value={mock} onChange={e => setMock(e.target.value)}/>
+        <button onClick={handleClick} disabled={mock === windowMock}>set</button>
+      </div>
     </h2>
   );
 }
--- packages/vue-demo/src/views/About.vue
+++ packages/vue-demo/src/views/About.vue
@@ -1,8 +1,35 @@
 <template>
   <div class="about">
     <h1>This is about page</h1>
+    <div>
+      <code>window.mock=</code>
+      <input type="text" v-model="mock">
+      <button @click="setValue" :disabled="disabled">set</button>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      mock: window.mock,
+      windowMock: window.mock
+    }
+  },
+  methods: {
+    setValue() {
+      window.mock = this.mock;
+      this.windowMock = this.mock;
+    }
+  },
+  computed: {
+    disabled() {
+      return this.mock === this.windowMock;
+    }
+  }
+}
+</script>
 
 <style scoped>
   .about {
